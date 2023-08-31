e59350b393ecee6e1d42d38110a2d8da88cca84e
100147919 <100147919@lubbockisd.net>
2023-08-31 08:32:57 -0500

    coding using println

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t\t\t |>>>");
+        System.out.println("\t\t\t\t\t\t |");
+        System.out.println("\t\t\t\t\t  _ _|_ _");
+        System.out.println("\t\t\t\t\t|;|_|;|_|;|");
+        System.out.println("\t\t\t\t\t\\\\:    . /");
+        System.out.println("\t\t\t\t\t \\\\: .  /");
+        System.out.println("\t\t\t\t\t  ||:  |");
+        System.out.println("\t\t\t\t\t  ||:. |");
+        System.out.println("\t\t\t\t\t  ||: .|");
+        System.out.println("\t\t\t\t\t  ||:  | \t\t\\,/");
+        System.out.println("\t\t\t\t\t  ||: ,| \t\t\t/'\\");
+        System.out.println("\t\t\t\t\t  ||:  |");
+        System.out.println("\t\t\t\t\t  ||:. |");
+        System.out.println("\t\t\t\t\t _||_  |");
+        System.out.println("\t\t\t  __----~    ~'---,\t\t\t\t ___");
+        System.out.println(" ~---__ ,--~'                  ~~----_____-~'   `~----~~ ");
+
+    }
+}
+
