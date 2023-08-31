38d483b20f224e8530f4c54228d802474385a210
100147919 <100147919@lubbockisd.net>
2023-08-31 08:47:12 -0500

    coding using println

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("\t\t\t\t\t\t |>>>");
         System.out.println("\t\t\t\t\t\t |");
         System.out.println("\t\t\t\t\t  _ _|_ _");
